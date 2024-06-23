38a9d55964ec77878f084b54d8fe2b16c9ed5fa4
malli031998 <vvdmallikarjunareddy@gmail.com>
2024-06-23 12:15:13 +0530

    Update DepartmentServiceApplication.java

--- src/main/java/com/example/Department_service/DepartmentServiceApplication.java
+++ src/main/java/com/example/Department_service/DepartmentServiceApplication.java
@@ -8,6 +8,9 @@ public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
+
+		// new changes
+		
 	}
 
 }
